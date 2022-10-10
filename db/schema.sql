919ddb357d7c332df3072fa129a2a10b379caedf
Shane <shanep42@gmail.com>
2022-10-10 16:36:07 -0500

    Expands schema with tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,32 @@
 DROP DATABASE IF EXISTS company_db;
 
 CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE
+    department (
+        id INT AUTO_INCREMENT,
+        name VARCHAR(30),
+        PRIMARY KEY (id)
+    );
+
+CREATE TABLE
+    role (
+        id INT AUTO_INCREMENT,
+        title VARCHAR(30),
+        salary DECIMAL,
+        department_id INT,
+        PRIMARY KEY (id)
+    );
+
+CREATE TABLE
+    employee (
+        id INT AUTO_INCREMENT,
+        first_name VARCHAR(30),
+        last_name VARCHAR(30),
+        role_id INT,
+        manager_id INT
+    );
+
+-- TODO: Shouldn't some of these use foreign keys?
--- index.js
+++ index.js
@@ -1,134 +1,137 @@
+require('dotenv').config();
 const inquirer = require('inquirer');
+const sequelize = require('./config/connection');
 
 
 function goToMainMenu() {
     inquirer
-    .prompt([
-        {
-            name: 'selection',
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
-        }
-    ])
-    .then ((answer) => {
-        switch (answer.selection) {
-            case 'View All Employees':
-                viewAllEmployees();
-                break;
-            case 'Add Employee':
-                addEmployee();
-                break;
-            case 'Update Employee Role':
-                updateEmployeeRole();
-                break;
-            case 'View All Roles':
-                viewAllRoles();
-                break;
-            case 'Add a Role':
-                addARole();
-                break;
-            case 'View All Departments':
-                viewAllDepartments();
-                break;
-            case 'Add Department':
-                addDepartment();
-                break;
-        }
-} )}
+        .prompt([
+            {
+                name: 'selection',
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+            }
+        ])
+        .then((answer) => {
+            switch (answer.selection) {
+                case 'View All Employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Update Employee Role':
+                    updateEmployeeRole();
+                    break;
+                case 'View All Roles':
+                    viewAllRoles();
+                    break;
+                case 'Add a Role':
+                    addARole();
+                    break;
+                case 'View All Departments':
+                    viewAllDepartments();
+                    break;
+                case 'Add Department':
+                    addDepartment();
+                    break;
+            }
+        })
+}
 
 function viewAllEmployees() {
     //TODO: display db data for: employee id, first name, last name, job title, department, salaries, manager
 }
 
-function addEmployee(){
+function addEmployee() {
     inquirer
-    .prompt([
-        {
-            name: 'firstName',
-            type: 'input',
-            message: 'Enter employee first name'
-        },{
-            name: 'lastName',
-            type: 'input',
-            message: 'Enter employee last name'
-        },{
-            name: 'empRole',
-            type: 'input',
-            // TODO: should maybe pull from the database and look for roles to display as options, instead of accepting any input?
-            message: 'Enter employee role'
-        },{
-            name: 'empManager',
-            type: 'input',
-            message: 'Enter manager'
-        }
-    ])
-    .then ((answers) => {
-        // TODO: write answers to db
-        console.log(answers)
-    })
+        .prompt([
+            {
+                name: 'firstName',
+                type: 'input',
+                message: 'Enter employee first name'
+            }, {
+                name: 'lastName',
+                type: 'input',
+                message: 'Enter employee last name'
+            }, {
+                name: 'empRole',
+                type: 'input',
+                // TODO: should maybe pull from the database and look for roles to display as options, instead of accepting any input?
+                message: 'Enter employee role'
+            }, {
+                name: 'empManager',
+                type: 'input',
+                message: 'Enter manager'
+            }
+        ])
+        .then((answers) => {
+            // TODO: write answers to db
+            console.log(answers)
+        })
 }
 
-function updateEmployeeRole(){
+function updateEmployeeRole() {
     inquirer
-    .prompt([
-        {
-            name: 'empSelection',
-            type: 'list',
-            message: 'Select an employee to update:',
-            //TODO: Get choices from database
-            choices: []
-        }
-    ])
+        .prompt([
+            {
+                name: 'empSelection',
+                type: 'list',
+                message: 'Select an employee to update:',
+                //TODO: Get choices from database
+                choices: []
+            }
+        ])
 }
 
-function viewAllRoles(){
+function viewAllRoles() {
     //TODO: Display roles from Database
 }
 
-function addARole(){
+function addARole() {
     inquirer
-    .prompt([
-        {
-            name: 'roleName',
-            type: 'input',
-            message: 'Enter role name'
-        },{
-            name: 'roleSalary',
-            type: 'input',
-            message: 'Enter salary'
-        },{
-            name: 'roleDepartment',
-            // TODO: Get roles from DB for list selection instead of input
-            type: 'input',
-            message: 'Enter department'
-        }
-    ])
-    .then ((answer) => {
-        //TODO: Add answers to Database
-        // console.log("Role added")
-        console.log("INCOMPLETE")
-        goToMainMenu();
-    })
+        .prompt([
+            {
+                name: 'roleName',
+                type: 'input',
+                message: 'Enter role name'
+            }, {
+                name: 'roleSalary',
+                type: 'input',
+                message: 'Enter salary'
+            }, {
+                name: 'roleDepartment',
+                // TODO: Get roles from DB for list selection instead of input
+                type: 'input',
+                message: 'Enter department'
+            }
+        ])
+        .then((answer) => {
+            //TODO: Add answers to Database
+            // console.log("Role added")
+            console.log("INCOMPLETE")
+            goToMainMenu();
+        })
 }
 
-function viewAllDepartments(){
+function viewAllDepartments() {
     //TODO: Display all departments currently in Database
 }
 
-function addDepartment(){
+function addDepartment() {
     inquirer
-    .prompt([
-        {
-            name: 'deptName',
-            type: 'input',
-            message: 'Enter department name'
-        }
-    ])
-    .then ((answer) => {
-        //TODO: Enter department into DB with auto-incremented ID
-        // console.log('Department Added')
-        console.log('INCOMPLETE');
-        goToMainMenu();
-    })
+        .prompt([
+            {
+                name: 'deptName',
+                type: 'input',
+                message: 'Enter department name'
+            }
+        ])
+        .then((answer) => {
+            //TODO: Enter department into DB with auto-incremented ID
+            // console.log('Department Added')
+            console.log('INCOMPLETE');
+            goToMainMenu();
+        })
 }
